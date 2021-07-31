343cd9709ccbeb812174053e43b97b09ddeabe3e
Pat Colyn <13235826+patcolyn@users.noreply.github.com>
2021-07-31 10:42:03 +0100

    Add delierioClicker

--- main.lua
+++ main.lua
@@ -1,39 +1,77 @@
-local deli = RegisterMod("Delierio", 1)
+local del = RegisterMod("delierio", 1)
 local rng = RNG() --RNG is non-inclusive
 local SaveState = {}
+
 
 ----------------------------------------------------------------
 --------------------------Default-Settings----------------------
 
 local modSettings = {
-
+	
 }
 
+
+----------------------------------------------------------------
+-------------------------------Init-----------------------------
+
+delClickerID = Isaac.GetItemIdByName("Delierio Clicker")
+
+function del:delInit()
+
+end
+
+
+----------------------------------------------------------------
+------------------------------Start-----------------------------
+
+function del:onGameStart()
+	local player  = Isaac.GetPlayer(0)
+	if player:GetName() == "Delierio" then
+		del:delInit()
+	end
+end
+del:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, del.onGameStart)
+
+
+----------------------------------------------------------------
+-----------------------------Clicker----------------------------
+
+function del:clicker(_item, _rng, player)
+	print(_item, _rng, player)
+	
+	
+	return true --Play clicker animation
+end
+del:AddCallback(ModCallbacks.MC_USE_ITEM, del.clicker, delClickerID, rng:RandomInt(18))
 
 ----------------------------------------------------------------
 ----------------------------Savedata----------------------------
 
 local json = require("json")
 
-function deli:SaveGame()
+function del:SaveGame()
+--Saves gamedata to /The Binding of Isaac Rebirth/data/delerio
+
 	SaveState.Settings = {}
 	
-	for i, v in pairs(deliSettings) do
-		SaveState.Settings[tostring(i)] = deliSettings[i]
+	for i, v in pairs(modSettings) do
+		SaveState.Settings[tostring(i)] = modSettings[i]
 	end
-    deli:SaveData(json.encode(SaveState))
+    del:SaveData(json.encode(SaveState))
 end
-deli:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, deli.SaveGame)
 
-function deli:OnGameStart(isSave)
-	deli:SaveGame()
+del:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, del.SaveGame)
+
+
+function del:loadData(isSave)
+--Loads gamedata from /The Binding of Isaac Rebirth/data/delerio
 	
-    if deli:HasData() then	
-		SaveState = json.decode(deli:LoadData())	
+    if del:HasData() then	
+		SaveState = json.decode(del:LoadData())	
 		
         for i, v in pairs(SaveState.Settings) do
-			deliSettings[tostring(i)] = SaveState.Settings[i]
+			modSettings[tostring(i)] = SaveState.Settings[i]
 		end
     end
 end
-deli:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, deli.OnGameStart)
+del:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, del.loadData)
