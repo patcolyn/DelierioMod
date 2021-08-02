67072a7398f37a96658eee1dacf84bd88816f502
Uwishiii <dylanwever2002@gmail.com>
2021-08-02 01:47:25 -0400

    Implementing Clicker Functionality + Code Cleanup

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+----------------------------------------------------------------
+-------------------------Importing-Tables-----------------------
+local tables = require("tables")
+
+----------------------------------------------------------------
+-----------------------Registering-Variables--------------------
+
 local del = RegisterMod("delierio", 1)
 local rng = RNG() --RNG is non-inclusive
 local SaveState = {}
@@ -35,26 +42,95 @@ del:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, del.onGameStart)
 
 ----------------------------------------------------------------
 -----------------------------Clicker----------------------------
+--[[
+local validPlayerTypes = {
+	dIsaac = 0,
+	dMagdalene = 1,
+	dCain = 2,
+	dJudas = 3,
+	dBlueBaby = 4,
+	dEve = 5,
+	dSamson = 6,
+	dAzazel = 7,
+	dLazarus = 8,
+	dEden = 9,
+	dTheLost = 10,
+	dLillith = 13,
+	dKeeper = 14,
+	dApollyon = 15,
+	dTheForgotten = 16,
+	dBethany = 18,
+	dJacob = 19
+}
+--]]
 
-validPlayerTypes = {}
+local validPlayerTypes = {
+	0,
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	13,
+	14,
+	15,
+	16,
+	18,
+	19
+}
+
+local spriteSheetLocations = {
+	"gfx/characters/costumes/character_isaac.png",
+	"gfx/characters/costumes/character_magdalene.png",
+	"gfx/characters/costumes/character_cain.png",
+	"gfx/characters/costumes/character_judas.png",
+	"gfx/characters/costumes/character_bluebaby.png",
+	"gfx/characters/costumes/character_eve.png",
+	"gfx/characters/costumes/character_samson.png",
+	"gfx/characters/costumes/character_azazel.png",
+	"gfx/characters/costumes/character_lazarus.png",
+	"gfx/characters/costumes/character_eden.png",
+	"gfx/characters/costumes/character_thelost.png",
+	"gfx/characters/costumes/character_lillith.png",
+	"gfx/characters/costumes/character_keeper.png",
+	"gfx/characters/costumes/character_apollyon.png",
+	"gfx/characters/costumes/character_theforgotten.png",
+	"gfx/characters/costumes/character_bethany.png",
+	"gfx/characters/costumes/character_jacob.png",
+}
 
 function del:clicker(_item, _rng, player)
 	--print(_item, _rng, player)
+	local randomIntFromTable = rng:RandomInt(#validPlayerTypes) -- Storing randomly chosen position from validPlayerTypes in randomIntFromTable
+	local randomPlayerType = validPlayerTypes[randomIntFromTable] -- Storing chosen playerType in randomPlayerType
+
+	table.remove(validPlayerTypes, randomIntFromTable)
+
+	player:ChangePlayerType(randomPlayerType) -- Changing PlayerType to a random validPlayerType
+	player:AddCollectible (delClickerID, 0, false, ActiveSlot.SLOT_POCKET) -- Adding Delierio Clicker to the transformed-into character as a pocket active
+
+	table.insert(validPlayerTypes, randomIntFromTable, randomPlayerType)
+
+	print("                                 Player: "..player:GetName()) -- Print who the player transformed into
+
+	local playerSprite = player:GetSprite() -- Storing GetSprite() as a var for easier use
+	playerSprite:Load("001.000_player.anm2", true) -- Loading player animations for changing spritesheet
 	
-	player:ChangePlayerType(rng:RandomInt(17))
-	player:AddCollectible (delClickerID, 0, false, ActiveSlot.SLOT_POCKET)
-	
-	print("                                 Player: "..player:GetName())
-	
-	playersprite = player:GetSprite()
-	playersprite:Load("001.000_player.anm2", true)
-	for i = 0, 15 do
-		playersprite:ReplaceSpritesheet(i, "gfx/characters/costumes/character_delierio.png")
+	for i = 1, 17 do
+		if randomPlayerType == validPlayerTypes[i] then -- Checking which character the player turned into
+			for ii = 0, 15 do -- Repeating 16 times for each player animation
+				playerSprite:ReplaceSpritesheet(ii, spriteSheetLocations[i]) -- Replacing spritesheet for every animation
+			end
+		end
 	end
-	playersprite:LoadGraphics()
-	
-	
-	return true --Play clicker animation
+
+	playerSprite:LoadGraphics() -- Refreshing the graphics to load the new spritesheet
+	return true -- Play clicker animation
 end
 del:AddCallback(ModCallbacks.MC_USE_ITEM, del.clicker, delClickerID, rng:RandomInt(18))
 
